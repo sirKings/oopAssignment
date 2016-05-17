731cbedfe1ff44adb35c615fcfb11ade7e0eed65
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-17 16:12:26 +0100

    Add the get and serach functionality

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ NotesApplication.prototype.create(note_content){
 }
 
 NotesApplication.prototype.listNotes(){
-	for(i = 0; i<this.notesList.length; i++){
-		console.log("Note ID: " +i)
-		console.log(this.notesList[i])
-		console.log("By Author " + this.author );
+	if(this.notesList.length>0){
+		for(i = 0; i<this.notesList.length; i++){
+			console.log("Note ID: " +i)
+			console.log(this.notesList[i])
+			console.log("By Author " + this.author );
+		}
 	}
-	
+}
+
+NotesApplication.prototype.get(note_id){
+	return this.notesList[note_id];
+}
+
+NotesApplication.prototype.search(search_text){
+	var result = "Showing results for search"
+	for(i=0; i<this.notesList.length; i++){
+		if(this.notesList.indexOf(search_text) !== -1){
+			
+		}
+	}
 }
