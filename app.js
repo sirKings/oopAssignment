751576a7c7e5bf0ef96f784782d8d535ce88d6b8
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-17 17:06:38 +0100

    made a very important syntax correction

--- app.js
+++ app.js
@@ -3,41 +3,41 @@ function NotesApplication(author) {
 	this.notesList = [];
 }
 
-NotesApplication.prototype.create(note_content){
-	this.notesList.push(note_content)
-}
+NotesApplication.create = function(note_content){
+	this.notesList.push(note_content);
+};
 
-NotesApplication.prototype.listNotes(){
+NotesApplication.prototype.listNotes = function(){
 	if(this.notesList.length>0){
 		for(i = 0; i<this.notesList.length; i++){
-			console.log("Note ID: " +i)
-			console.log(this.notesList[i])
+			console.log("Note ID: " +i);
+			console.log(this.notesList[i]);
 			console.log("By Author " + this.author );
 		}
 	}
-}
+};
 
-NotesApplication.prototype.get(note_id){
+NotesApplication.prototype.get = function(note_id){
 	console.log(this.notesList[note_id]);
-}
+};
 
-NotesApplication.prototype.search(search_text){
-	var result = "Showing results for search"
+NotesApplication.prototype.search = function(search_text){
+	var result = "Showing results for search";
 	for(i=0; i<this.notesList.length; i++){
 		if(this.notesList.indexOf(search_text) !== -1){
 			note = "Notes ID: " +i+ "\n" + this.notesList[i] + "\n" +"By Author "+ author;
 		}	result += note;
 	}
-	console.log(result)
-}
+	console.log(result);
+};
 
-NotesApplication.prototype.delete(note_id){
+NotesApplication.prototype.delete = function(note_id){
 	if(this.notesList.length> note_id)
-	this.notesList.splice(note_id, 1)
-}
+	this.notesList.splice(note_id, 1);
+};
 
-NotesApplication.prototype.edit(note_id, new_content){
+NotesApplication.prototype.edit = function(note_id, new_content){
 	if(this.notesList.length > note_id){
 		this.notesList[note_id] = new_content;
 	}
-}
+};
