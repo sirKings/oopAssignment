6cd151e5fc7f294b1c9e49a9cd5c85c932d786f3
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2016-05-17 15:52:45 +0100

    create the listNote and create functonality

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ function NotesApplication(author) {
 }
 
 NotesApplication.prototype.create(note_content){
+	this.notesList.push(note_content)
+}
+
+NotesApplication.prototype.listNotes(){
+	for(i = 0; i<this.notesList.length; i++){
+		console.log("Note ID: " +i)
+		console.log(this.notesList[i])
+		console.log("By Author " + this.author );
+	}
 	
 }
